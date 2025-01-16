b8c65dd6d0434e485e20c434dbd94bbbb72948b0
Rasheed <Raadebowale@gmail.com>
2025-01-16 14:00:34 +0100

    Updated: Projects and Cv

--- testimonials.js
+++ testimonials.js
@@ -1,7 +1,7 @@
 const testimonials = [
   {
     quote:
-      "Working with Rasheed was a game-changer for our project. Their coding skills are exceptional, and the innovative solutions they provided exceeded our expectations. Their commitment to deadlines and communication prowess made the entire development process a seamless experience",
+      "Working with Rasheed was a game-changer for our project. His coding skills are exceptional, and the innovative solutions he provided exceeded our expectations. His commitment to deadlines and communication prowess made the entire development process a seamless experience",
     avatar: "userProfile",
     name: "Popoola Philip",
     title: "Web Developer",
@@ -9,15 +9,15 @@ const testimonials = [
 
   {
     quote:
-      "Rasheed is a coding virtuoso! Their ability to turn complex ideas into elegant, efficient code is unmatched. The collaborative spirit and commitment to understanding our unique needs made the development process a pleasure. Looking forward to many more projects together!",
+      "Rasheed loves solving problems! His ability to turn complex ideas into elegant, efficient code is unmatched. His collaborative spirit and commitment to understanding our unique needs made the development process a pleasure. Looking forward to many more projects together!",
     avatar: "userProfile",
     name: "Praise Nwanchukwu",
-    title: "Data Analyst",
+    title: "Digital Marketer",
   },
 
   {
     quote:
-      "Exceptional talent alert! Rasheed brought unparalleled expertise to our development team. Their knack for innovation, coupled with a disciplined approach to coding, transformed our project. The results speak for themselves. A top-tier developer, no doubt!",
+      "Rasheed brought unparalleled expertise to our development team. His knack for innovation, coupled with a disciplined approach to coding, transformed our project. The results speak for themselves. A top-tier developer, no doubt!",
     avatar: "userProfile",
     name: "Adebayo Motunrayo",
     title: "Human Resource Manager",
@@ -25,7 +25,7 @@ const testimonials = [
 
   {
     quote:
-      "I can't express how fortunate we were to have Rasheed on board. The coding brilliance and meticulous attention to detail elevated our project. What sets them apart is not just technical prowess but also an incredible ability to communicate complex concepts. Highly recommended!",
+      "I can't express how fortunate we were to have Rasheed on board. The coding brilliance and meticulous attention to detail elevated our project. What sets him apart is not just technical prowess but also an incredible ability to communicate. Highly recommended!",
     avatar: "userProfile",
     name: "Ibrahim Bolanle",
     title: "Banker",
